e819c7da2205297c0a92a8cc13668a83f9d1edbf
Marian <33843056+bmarian@users.noreply.github.com>
2020-09-27 14:35:03 +0300

    Buttons implementation

--- src/module/windows/GenericWindow.ts
+++ src/module/windows/GenericWindow.ts
@@ -7,6 +7,10 @@ abstract class GenericWindow {
     protected _$window: any;
     protected _options: any;
     protected _buttons: any;
+    protected _lastPosition: any = {};
+    protected _constObjects = {
+        $canvas: $(this._defaultClasses.CANVAS_QRY),
+    }
 
     constructor(windowObj: any, $window: any, options: any) {
         this._windowObj = windowObj;
@@ -15,39 +19,13 @@ abstract class GenericWindow {
         this._hook();
     }
 
-    private _generateButtonHTML(cls: string, type: string): string {
-        return `<a class="${cls}"><i class="fas ${type}"></i></a>`;
-    }
-
-    protected _generateButtons(): void {
-        const $closeButton = this._$window.find(this._defaultClasses.CLOSE_BUTTON_QRY);
-        const $maximizeButton = $(this._generateButtonHTML('resize-max', 'fa-window-maximize'));
-        const $restoreButton = $(this._generateButtonHTML('resize-restore', 'fa-window-restore'));
-        const $minimizeButton = $(this._generateButtonHTML('minimize', 'fa-window-minimize'));
-        this._buttons = {$closeButton, $maximizeButton, $restoreButton, $minimizeButton};
-
-        Utils.debug('GW button generation', this._buttons);
-    }
-
-    protected _addButtons(): void {
-        this._buttons.$closeButton.before(this._buttons.$minimizeButton);
-
-        // Only add the maximize button if the window is resizable
-        if (this._windowObj?.options?.resizable) this._buttons.$closeButton.before(this._buttons.$maximizeButton);
-
-        // This should remove the text for the close button (to look more like a Windows window)
-        this._buttons.$closeButton.contents().last().remove();
-
-        Utils.debug('GW buttons added');
-    }
-
     protected _hijackedMinimize(ev): Promise<boolean> {
         // Prevent minimizing on double clicking the header
-        if ((ev.target.className === this._defaultClasses.WINDOW_TITLE ||
-            ev.target.className.contains(this._defaultClasses.WINDOW_HEADER)) &&
+        if ((ev?.target?.className === this._defaultClasses.WINDOW_TITLE ||
+            ev?.target?.className.includes(this._defaultClasses.WINDOW_HEADER)) &&
             ev.type === 'dblclick') return;
 
-        if ( !this._windowObj.popOut || [true, null].includes(this._windowObj._minimized) ) return;
+        if (!this._windowObj.popOut || [true, null].includes(this._windowObj._minimized)) return;
         this._windowObj._minimized = null;
 
         // Get content
@@ -63,7 +41,7 @@ abstract class GenericWindow {
 
         // Slide up window height
         return new Promise((resolve) => {
-            window.animate({height: `${header[0].offsetHeight+1}px`}, 100, () => {
+            window.animate({height: `${header[0].offsetHeight + 1}px`}, 100, () => {
                 header.children().not(".window-title").not(".close").hide();
                 window.animate({width: MIN_WINDOW_WIDTH}, 100, () => {
                     window.addClass("minimized");
@@ -75,7 +53,7 @@ abstract class GenericWindow {
     }
 
     protected _hijackedMaximize(ev): Promise<boolean> {
-        if ( !this._windowObj.popOut || [false, null].includes(this._windowObj._minimized) ) return;
+        if (!this._windowObj.popOut || [false, null].includes(this._windowObj._minimized)) return;
         this._windowObj._minimized = null;
 
         // Get content
@@ -85,7 +63,10 @@ abstract class GenericWindow {
 
         // Expand window
         return new Promise((resolve) => {
-            window.animate({width: this._windowObj.position.width, height: this._windowObj.position.height}, 100, () => {
+            window.animate({
+                width: this._windowObj.position.width,
+                height: this._windowObj.position.height
+            }, 100, () => {
                 header.children().show();
                 content.slideDown(100, () => {
                     window.removeClass("minimized");
@@ -99,7 +80,7 @@ abstract class GenericWindow {
     }
 
     // @ts-ignore
-    protected _hijackedSetPosition({left, top, width, height, scale}={}) {
+    protected _hijackedSetPosition({left, top, width, height, scale} = {}) {
         if (!this._windowObj.popOut) return; // Only configure position for popout apps
         const el = this._windowObj.element[0];
         const p = this._windowObj.position;
@@ -153,7 +134,7 @@ abstract class GenericWindow {
 
         // Update Top
         if ((pop && !el.style.top) || Number.isFinite(top)) {
-            const headerHeight = el.querySelector(`.${this._defaultClasses.WINDOW_HEADER}`)?.offsetHeight;
+            const headerHeight = Number(el.querySelector(`.${this._defaultClasses.WINDOW_HEADER}`)?.offsetHeight) || 0;
             const maxTop = Math.max(window.innerHeight - headerHeight, 0);
 
             if (!Number.isFinite(top)) top = (window.innerHeight - el.offsetHeight) / 2;
@@ -184,26 +165,105 @@ abstract class GenericWindow {
         Utils.debug('GW hijacked events');
     }
 
-    protected _minimizeEvent(): void {
-
+    private _generateButtonHTML(cls: string, type: string): string {
+        return `<a class="${cls}"><i class="fas ${type}"></i></a>`;
     }
 
-    protected _resizeEvent(): void {
+    protected _generateButtons(): void {
+        const $closeButton = this._$window.find(this._defaultClasses.CLOSE_BUTTON_QRY);
+        const $maximizeButton = $(this._generateButtonHTML('resize-max', 'fa-window-maximize'));
+        const $restoreButton = $(this._generateButtonHTML('resize-restore', 'fa-window-restore'));
+        const $minimizeButton = $(this._generateButtonHTML('minimize', 'fa-window-minimize'));
+        this._buttons = {$closeButton, $maximizeButton, $restoreButton, $minimizeButton};
+
+        Utils.debug('GW buttons generated', this._buttons);
+    }
+
+    protected _addButtons(): void {
+        this._buttons.$closeButton.before(this._buttons.$minimizeButton);
+
+        // Only add the maximize button if the window is resizable
+        if (this._windowObj?.options?.resizable) this._buttons.$closeButton.before(this._buttons.$maximizeButton);
+
+        // This should remove the text for the close button (to look more like a Windows window)
+        this._buttons.$closeButton.contents().last().remove();
+
+        Utils.debug('GW buttons added');
+    }
+
+    /**
+     * Event listener for `mousedown` to prevent moving while holding the newly added buttons
+     * @protected
+     */
+    protected _preventMovement(ev: any): void {
+        ev.preventDefault();
+        ev.stopPropagation();
+    }
+
+    protected _minimizeEvent(ev: any): void {
+        this._windowObj.minimize(ev);
+    }
+
+    protected _resizeMaxEvent(ev: any, $maximizeButton: JQuery): void {
+        $maximizeButton.replaceWith(this._buttons.$restoreButton);
+
+        // Rebind the lost events
+        this._buttons.$restoreButton.on('mousedown', this._preventMovement);
+        this._buttons.$restoreButton.on('click', this._resizeEvent.bind(this));
+
+        this._lastPosition = {...this._windowObj.setPosition()};
+        const newPosition = {
+            width: Number(this._constObjects.$canvas.attr('width')),
+            height: Number(this._constObjects.$canvas.attr('height')),
+            left: 0,
+            top: 0,
+        }
+        this._windowObj.setPosition(newPosition);
+        this._$window.css(newPosition);
+
+        Utils.debug('Maximized: ', {oldPosition: this._lastPosition}, {newPosition});
+    }
+
+    protected _resizeRestoreEvent(ev: any, $restoreButton: JQuery): void {
+        $restoreButton.replaceWith(this._buttons.$maximizeButton);
+
+        // Rebind the lost events
+        this._buttons.$maximizeButton.on('mousedown', this._preventMovement);
+        this._buttons.$maximizeButton.on('click', this._resizeEvent.bind(this));
+
+        this._windowObj.setPosition(this._lastPosition);
+        this._$window.css(this._lastPosition);
+
+        Utils.debug('Restored: ', {oldPosition: this._lastPosition});
+    }
+
+    protected _resizeEvent(ev: any): void {
         // No need to add resize events if the window is not resizable
         if (!this._windowObj?.options?.resizable) return;
+        const $target = $(ev.target);
+        const $closestMax = $target.closest('.resize-max');
+        const $parent = $closestMax.length ? $closestMax : $target.closest('.resize-restore');
+        if (!$parent.length) return;
+
+        return $parent.hasClass('resize-max') ? this._resizeMaxEvent(ev, $parent) : this._resizeRestoreEvent(ev, $parent);
     }
 
     // Not implemented, might be needed in the future
-    protected _closeEvent(): void {}
+    protected _closeEvent(ev: Event, context: any): void {
+    }
 
     protected _addButtonEvents(): void {
-        this._hijackEvents();
-        this._minimizeEvent();
-        this._resizeEvent();
-        this._closeEvent();
+        this._buttons.$minimizeButton.on('mousedown', this._preventMovement);
+        this._buttons.$maximizeButton.on('mousedown', this._preventMovement);
+
+        this._buttons.$minimizeButton.on('click', this._minimizeEvent.bind(this));
+        this._buttons.$maximizeButton.on('click', this._resizeEvent.bind(this));
+
+        this._buttons.$closeButton.on('click', this._closeEvent.bind(this));
     }
 
     protected _hook(): void {
+        this._hijackEvents();
         this._generateButtons();
         this._addButtons();
         this._addButtonEvents();
